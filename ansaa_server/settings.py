4426513e194271aadb30f3d0c85dbe59450fb99d
Ng-Harry <ngereharry5@gmail.com>
2024-05-07 16:28:42 +0100

    remove smtp config

--- ansaa_server/settings.py
+++ ansaa_server/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # custom installed apps
     'authentication',
     'phonenumber_field',
     'rest_framework',
@@ -116,7 +115,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
