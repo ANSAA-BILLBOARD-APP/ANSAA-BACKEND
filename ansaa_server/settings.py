cb6f726baaddc424743b10dd5c35b817b4801327
Ng-Harry <ngereharry5@gmail.com>
2024-05-27 13:06:05 +0100

    custom domain config

--- ansaa_server/settings.py
+++ ansaa_server/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insegergrfdgvq w 4r35y6745rab1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh','www.dotsassets.com','dotsassets.com']
 
 
 # Application definition
