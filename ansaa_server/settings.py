4af6025cc14a27a3ba30ccfe2d2ab847611242ee
Ng-Harry <ngereharry5@gmail.com>
2024-06-05 14:23:32 +0100

    add eviron variables

--- ansaa_server/settings.py
+++ ansaa_server/settings.py
@@ -174,12 +174,14 @@ SIMPLE_JWT = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ecclesiafarmz@gmail.com'
-EMAIL_HOST_PASSWORD = 'snfu jygi cnhr klvv'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
